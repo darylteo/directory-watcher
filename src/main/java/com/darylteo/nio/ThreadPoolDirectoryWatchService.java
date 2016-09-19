4fa53ffda516f2011813bd739df94525df8761ad
Daryl Teo <dteo@daryl.teo>
2016-09-19 21:10:33 +1000

    Manual merge of #4

--- src/main/java/com/darylteo/nio/ThreadPoolDirectoryWatchService.java
+++ src/main/java/com/darylteo/nio/ThreadPoolDirectoryWatchService.java
@@ -2,27 +2,32 @@ package com.darylteo.nio;
 
 import java.io.IOException;
 import java.nio.file.ClosedWatchServiceException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * <p>
  * This implementation of DirectoryWatchService automatically waits for changes
  * using a Thread Pool.
  * </p>
- * 
- * @author Daryl Teo
+ *
+ * @author Daryl Teo <i.am@darylteo.com>
  * @see AbstractDirectoryWatchService
  * @see DirectoryWatcher
  */
 public class ThreadPoolDirectoryWatchService extends AbstractDirectoryWatchService {
+
   private final ExecutorService executorService = Executors.newCachedThreadPool();
+  private final List<WatcherThread> watcherThreads = new ArrayList<>();
 
   /**
    * <p>
    * Instantiates a DirectoryWatcherFactory with a single thread.
    * </p>
-   * 
+   *
    * @throws IOException
    */
   public ThreadPoolDirectoryWatchService() throws IOException {
@@ -33,14 +38,16 @@ public class ThreadPoolDirectoryWatchService extends AbstractDirectoryWatchServi
    * <p>
    * Instantiates a DirectoryWatcherFactory with a provided thread count.
    * </p>
-   * 
+   *
    * @param threadCount
    *          - number of threads to spawn for this factory.
    * @throws IOException
    */
   public ThreadPoolDirectoryWatchService(int threadCount) throws IOException {
     for (int i = 0; i < threadCount; i++) {
-      executorService.execute(new WatcherThread());
+      WatcherThread thread = new WatcherThread();
+      watcherThreads.add(thread);
+      executorService.execute(thread);
     }
   }
 
@@ -48,9 +55,18 @@ public class ThreadPoolDirectoryWatchService extends AbstractDirectoryWatchServi
    * Thread responsible for the watching logic
    */
   private class WatcherThread implements Runnable {
+
+    private AtomicBoolean stop = new AtomicBoolean(false);
+
+    public WatcherThread() {}
+
+    public void setStop(boolean stop) {
+      this.stop.set(stop);
+    }
+
     @Override
     public void run() {
-      while (true) {
+      while (!stop.get()) {
         try {
           ThreadPoolDirectoryWatchService.super.handleWatchKey(ThreadPoolDirectoryWatchService.super.getWatchService().take());
         } catch (InterruptedException | ClosedWatchServiceException e) {
@@ -59,10 +75,13 @@ public class ThreadPoolDirectoryWatchService extends AbstractDirectoryWatchServi
       }
     }
   }
-  
+
   @Override
   public void close() throws Exception {
-    executorService.shutdownNow();
     super.close();
+    executorService.shutdownNow();
+    for (WatcherThread thread : watcherThreads) {
+      thread.setStop(true);
+    }
   }
 }
